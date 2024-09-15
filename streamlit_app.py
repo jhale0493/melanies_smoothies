7c8673968ffddb839f90776b51b42b16b4af9470
jhale0493 <72719403+jhale0493@users.noreply.github.com>
2024-09-15 00:19:22 -0400

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ name_on_order = st.text_input("Name on Soothie")
 st.write("The name on your Soothie will be", name_on_order)
 
 #session = get_active_session()
-cnx = st.connection("snopwflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
